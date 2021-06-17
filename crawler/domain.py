b6340b8915e77925d0c794cc4d1784065b6b2c84
Thiago Fernandes Dornelles <thiago.fdornelles@gmail.com>
2021-06-17 07:39:49 -0300

    inicio da implementação da classe Domain

--- crawler/domain.py
+++ crawler/domain.py
@@ -1,28 +1,43 @@
 from datetime import datetime
+import sched
+import time
+
 
 class Domain():
-	def __init__(self,nam_domain,int_time_limit_between_requests):
-		self.time_last_access = datetime(1970,1,1)
-		self.nam_domain = nam_domain
-		self.int_time_limit_seconds  = int_time_limit_between_requests
-	@property
-	def time_since_last_access(self):
-		pass
+    def __init__(self, nam_domain, int_time_limit_between_requests):
+        self.time_last_access = datetime(1970, 1, 1)
+        self.nam_domain = nam_domain
+        self.int_time_limit_seconds = int_time_limit_between_requests
+        self._is_accessible = True
 
-	def accessed_now(self):
-		pass
+    @property
+    def time_since_last_access(self):
+        timedelta = datetime.now() - self.time_last_access
+        return timedelta
 
-	def is_accessible(self):
-		return False
+    def accessed_now(self):
+        def turn_server_accessible(self):
+            self._is_accessible = True
 
-	def __hash__(self):
-		return None
+        s = sched.scheduler(time.time, time.sleep)
+        self._is_accessible = False
 
-	def __eq__(self, domain):
-		return None
+        s.enter(self.int_time_limit_seconds, 1,
+                turn_server_accessible, argument={self})
+        s.run()
+        self.time_last_access = datetime.now()
 
-	def __str__(self):
-		return self.nam_domain
+    def is_accessible(self):
+        return self._is_accessible
 
-	def __repr__(self):
-		return str(self)
+    def __hash__(self):
+        return 5
+
+    def __eq__(self, domain):
+        return None
+
+    def __str__(self):
+        return self.nam_domain
+
+    def __repr__(self):
+        return str(self)

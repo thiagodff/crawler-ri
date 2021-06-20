33cacc730ec6052ed7e21845e76bac61bfe4c469
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2021-06-20 20:27:37 -0300

    [+] Solution 5, python code style and super call in PageFetcher

--- crawler/domain.py
+++ crawler/domain.py
@@ -1,32 +1,28 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from threading import Timer
 
 
-class Domain():
+class Domain:
     def __init__(self, nam_domain, int_time_limit_between_requests):
         self.time_last_access = datetime(1970, 1, 1)
         self.nam_domain = nam_domain
         self.int_time_limit_seconds = int_time_limit_between_requests
-        self._is_accessible = True
+        self.__accessible = True
 
     @property
-    def time_since_last_access(self):
-        timedelta = datetime.now() - self.time_last_access
-        return timedelta
+    def time_since_last_access(self) -> timedelta:
+        return datetime.now() - self.time_last_access
 
-    def turn_server_accessible(self):
-        self._is_accessible = True
+    def __turn_server_accessible(self) -> None:
+        self.__accessible = True
 
-    def accessed_now(self):
-        self._is_accessible = False
-
-        r = Timer(self.int_time_limit_seconds, self.turn_server_accessible)
-        r.start()
-
+    def accessed_now(self) -> None:
+        self.__accessible = False
         self.time_last_access = datetime.now()
+        Timer(self.int_time_limit_seconds, self.__turn_server_accessible).start()
 
-    def is_accessible(self):
-        return self._is_accessible
+    def is_accessible(self) -> bool:
+        return self.__accessible
 
     def __hash__(self):
         return hash(self.nam_domain)
